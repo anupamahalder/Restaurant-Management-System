8887ee2e9164519f05dd4926456281bdb3eb1e02
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-05-04 13:09:36 +0530

    Created an Admin class by inheriting User base class

---
+++ Admin_Class.py
@@ -0,0 +1,16 @@
+class Admin(User):
+    def __init__(self, name, email, phone, address):
+        super().__init__(name, email, phone, address)
+
+    def add_employee(self, restaurant, employee):
+        # name of the restaurant will be passed 
+        restaurant.add_employee(employee)
+
+    def view_employee(self, restaurant):
+        restaurant.view_employee()
+    
+    def add_new_item(self, restaurant, item):
+        restaurant.menu.add_menu_item(item)
+        
+    def remove_menu_item(self, restaurant, item):
+        restaurant.menu.remove_menu_item(item)
